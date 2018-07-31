3543a08ac4bbd41ae4f050d6949e52275bab3a02
initLiu90 <xianyue_pan@yeah.net>
2018-07-31 21:19:18 +0800

    基础网络库修改

--- library/src/main/java/com/lzp/library/net/NetCenter.java
+++ library/src/main/java/com/lzp/library/net/NetCenter.java
@@ -2,10 +2,19 @@ package com.lzp.library.net;
 
 import com.lzp.library.net.api.ApiService;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.MediaType;
@@ -44,6 +53,7 @@ public class NetCenter {
 
         mRetrofit = new Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClient)
                 //接口中通过@url指定了请求地址，所以这里baseurl没有用了，但是为了避免
                 //Retrofit.java
@@ -58,30 +68,30 @@ public class NetCenter {
         mApiService = mRetrofit.create(ApiService.class);
     }
 
-    public <R> Observable<R> getApiService(final RequestParams<?, ? super R> params) {
-        Observable<ResponseBody> observable = null;
-
+    public <T, R> Observable<R> getApiService(final RequestParams<T, R> params) {
+        Observable<ResponseBody> apiObservable = null;
         if (RequestParams.HttpMethod.GET.equals(params.httpMethod)) {//get
-            observable = mApiService.get(params.url, params.headers, params.params);
+            apiObservable = mApiService.get(params.url, params.headers, params.params);
         } else if (RequestParams.HttpMethod.POST.equals(params.httpMethod)
-                &&RequestParams.ContentType.APPLICATION_FORM_URLENCODED.equals(params.contentType)) {//post application/x-www-form-urlencoded
-            observable = mApiService.post(params.url, params.headers, params.params);
-        }else if (RequestParams.HttpMethod.POST.equals(params.httpMethod)
-                &&RequestParams.ContentType.APPLICATION_JSON.equals(params.contentType)){//post application/json
-            RequestBody.create(MediaType.parse(params.contentType),params.converter.requestConvert(params.params));
-
+                && RequestParams.ContentType.APPLICATION_FORM_URLENCODED.equals(params.contentType)) {//post application/x-www-form-urlencoded
+            apiObservable = mApiService.post(params.url, params.headers, params.params);
+        } else if (RequestParams.HttpMethod.POST.equals(params.httpMethod)
+                && RequestParams.ContentType.APPLICATION_JSON.equals(params.contentType)) {//post application/json
+            RequestBody requestBody = RequestBody.create(MediaType.parse(params.contentType), params.converter.requestConvert(params.requestBody));
+            apiObservable = mApiService.post(params.url, params.headers, requestBody);
         }
 
-        return observable.subscribeOn(Schedulers.io())
+        return apiObservable.subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
                 .map(new Function<ResponseBody, R>() {
                     @Override
                     public R apply(ResponseBody responseBody) throws Exception {
-                        return (R) params.converter.responseBodyConvert(responseBody.bytes());
+                        return (R) params.converter.responseConvert(responseBody.bytes());
                     }
                 })
                 .observeOn(AndroidSchedulers.mainThread());
     }
+
 
     private static class Config {
         public static long connectTimeout = 10 * 1000;
--- library/src/main/java/com/lzp/library/net/RequestParams.java
+++ library/src/main/java/com/lzp/library/net/RequestParams.java
@@ -1,38 +1,46 @@
 package com.lzp.library.net;
 
+import android.text.TextUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class RequestParams<F, T> {
+public class RequestParams<I, O> {
     public final String httpMethod;
     public final String url;
     public final Map<String, String> headers;
     public final Map<String, String> params;
     public final String contentType;
-    public final Converter<? super F, ? super T> converter;
+    public final Converter<? super I, ? super O> converter;
+    public final I requestBody;
 
-    public RequestParams(Builder builder) {
+    public RequestParams(Builder<I, O> builder) {
         this.httpMethod = builder.httpMethod;
         this.url = builder.url;
         this.headers = builder.headers;
         this.params = builder.params;
         this.converter = builder.converter;
         this.contentType = builder.contentType;
+        this.requestBody = builder.requestBody;
     }
 
 
-    public static class Builder<F, T> {
+    public static <I, O> Builder<I, O> creat(Converter<I, O> converter) {
+        return new Builder<>(converter);
+    }
+
+    public static class Builder<I, O> {
         private String httpMethod;
         private Map<String, String> headers = new HashMap<>();
         private Map<String, String> params = new HashMap<>();
         private String url;
 
-        private Convert<? super F, ? super T> converter;
+        private Converter<? super I, ? super O> converter;
         private String contentType = ContentType.APPLICATION_FORM_URLENCODED;
+        private I requestBody;
 
-        public Builder(String url, String httpMethod) {
-            this.url = url;
-            this.httpMethod = httpMethod;
+        private Builder(Converter<? super I, ? super O> converter) {
+            this.converter = converter;
         }
 
         private Builder() {
@@ -42,47 +50,52 @@ public class RequestParams<F, T> {
          * @param httpMethod {@link HttpMethod}
          * @return
          */
-        public Builder<F, T> method(String httpMethod) {
+        public Builder<I, O> method(String httpMethod) {
             this.httpMethod = httpMethod;
             return this;
         }
 
-        public Builder<F, T> addHeader(String key, String value) {
+        public Builder<I, O> addHeader(String key, String value) {
             headers.put(key, value);
             return this;
         }
 
-        public Builder<F, T> addHeader(Map<String, String> headers) {
+        public Builder<I, O> addHeader(Map<String, String> headers) {
             this.headers.putAll(headers);
             return this;
         }
 
-        public Builder<F, T> addParam(String key, String value) {
+        public Builder<I, O> addParam(String key, String value) {
             this.params.put(key, value);
             return this;
         }
 
-        public Builder<F, T> addParam(Map<String, String> params) {
+        public Builder<I, O> addParam(Map<String, String> params) {
             this.params.putAll(params);
             return this;
         }
 
-        public Builder<F, T> url(String url) {
+        public Builder<I, O> requestBody(I requestBody) {
+            this.requestBody = requestBody;
+            return this;
+        }
+
+        public Builder<I, O> url(String url) {
             this.url = url;
             return this;
         }
 
-        public Builder<F, T> contentType(String contentType) {
+        public Builder<I, O> contentType(String contentType) {
             this.contentType = contentType;
             return this;
         }
 
-        public Builder<F, T> converter(Convert<? super F, ? super T> converter) {
-            this.converter = converter;
-            return this;
-        }
+        public RequestParams<I, O> build() {
+            if (TextUtils.isEmpty(url))
+                throw new RuntimeException("url should not be null");
+            if (TextUtils.isEmpty(httpMethod))
+                throw new RuntimeException("httpMethod should not be null");
 
-        public RequestParams<F, T> build() {
             return new RequestParams(this);
         }
     }
